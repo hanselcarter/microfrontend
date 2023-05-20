a93fe470f05ea26ecd1581a045292febc985445e
Hansel Carter <barahonahansel@hotmail.com>
2023-05-20 11:57:03 -0600

    chore: scaffolding

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,39 @@
+const { merge } = require("webpack-merge");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const commonConfig = require("./webpack.common");
+
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const packageJson = require("../package.json");
+
+const devConfig = {
+    mode: "development",
+    devServer: {
+        port: 8080,
+        historyApiFallback: {
+            index: "index.html",
+        },
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            //This is the host name
+            name: "container",
+            remotes: {
+                //remember this name has to match up with the one declared o exposed on our marketing app
+                marketing: "marketing@http://localhost:8081/remoteEntry.js",
+            },
+            //Big modules like react we should only load one copy, make sure both apps run the same version,
+            //this can also be enforce by using singleton prop also this can be delegated to webpack but you might want very specific and use this manual way too
+            // shared: ["react", "react-dom"],
+
+            //Delegated way,
+            shared: packageJson.dependencies,
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import {  } from "react-router-dom";
+//Mount function to start up the app
+
+const mount = (el) => {
+    ReactDOM.render(<App />, el);
+};
+
+//If we are in development and in isolation
+//call mount immediately
+
+if (process.env.NODE_ENV === "development") {
+    const devRoot = document.querySelector("#_marketing-dev-root");
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+//We are running though a container
+//and we should export the mount function
+
+export { mount };
