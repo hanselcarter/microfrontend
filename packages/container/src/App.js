5f886a4735f2ebc749fa880e9fe8897f42d58bf1
Hansel Carter <barahonahansel@hotmail.com>
2023-05-20 13:34:49 -0600

    chore: fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return (
         <div>
-            <h1>Hi mfe</h1>
+            <h1>Hi mf1e</h1>
             <hr />
             <MarketingApp />
         </div>
