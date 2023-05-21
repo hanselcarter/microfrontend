0d048dbc85cca32e26063ab44576d480c5ebb023
Hansel Carter <barahonahansel@hotmail.com>
2023-05-21 17:06:20 -0600

    chore: fix css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+//This how we fix css classnames collisions we make sure we add this prefix
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+});
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <hr />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <hr />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+//This how we fix css classnames collisions we make sure we add this prefix
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+});
+
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing} />
