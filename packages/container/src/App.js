ab65d5e66b43796cefbf27b6e53c610951f93c82
Hansel Carter <barahonahansel@hotmail.com>
2023-05-20 19:10:52 -0600

    chore: re-deploy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return (
         <div>
-            <h1>Hi mf</h1>
+            <h1>Hi container</h1>
             <hr />
             <MarketingApp />
         </div>
